bae5de256934b23993ac218d6ada00f31f3128e0
pinyin <bob432091@gmail.com>
2018-07-16 00:00:47 +0800

    0.0.8

--- src/nextFrame.ts
+++ src/nextFrame.ts
@@ -1,6 +1,6 @@
 import {onFrameStart} from 'framesync'
 import {SynchronousPromise} from 'synchronous-promise'
 
-export async function nextFrame(): SynchronousPromise<void> {
-    return new SynchronousPromise<void>(resolve => onFrameStart(resolve))
+export async function nextFrame(): Promise<void> {
+    return new SynchronousPromise<void>(resolve => onFrameStart(resolve, false))
 }
--- src/readPhase.ts
+++ src/readPhase.ts
@@ -1,6 +1,6 @@
 import {onFrameStart} from 'framesync'
 import {SynchronousPromise} from 'synchronous-promise'
 
-export async function readPhase(): SynchronousPromise<void> {
-    return new SynchronousPromise<void>(resolve => onFrameStart(resolve))
+export async function readPhase(): Promise<void> {
+    return new SynchronousPromise<void>(resolve => onFrameStart(resolve, true))
 }
